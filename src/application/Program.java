d19ca557a54bda6ac5894dcbffdfd61111b4613c
LorenaFFC <fernandescunhalorena3@gmail.com>
2020-12-01 23:18:59 -0300

    Deploy

---
+++ src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);  // inclui o "." como separador de decimais
+		Scanner sc = new Scanner(System.in);
+		Account account;
+		
+		
+		System.out.println("Enter account number: ");
+		int number = sc.nextInt();
+		
+		System.out.println("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		
+		System.out.println("Is there na initial deposit (y/n)?");
+		char response = sc.next().charAt(0);		
+		
+		if(response =='y') {
+			System.out.println("Enter initial deposit value:   ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);	 
+		}
+		else {
+			account = new Account(number, holder);	
+		}
+		
+		System.out.println();
+		System.out.println("Account data:  ");
+		System.out.println(account.tostring());
+		
+		
+		// DEPOSITO
+		System.out.println();
+		System.out.println("Enter a deposit value: ");
+		double depositeValue = sc.nextDouble();
+		account.deposit(depositeValue);
+		
+		System.out.println("Updated account data:  ");
+		System.out.println(account.tostring());
+		
+		// SAQUE
+		System.out.println();
+		System.out.println("Enter a withdraw value: ");
+		double withdrawvalue = sc.nextDouble();
+		account.withdraw(withdrawvalue);
+		
+		System.out.println("Updated account data:  ");
+		System.out.println(account.tostring());
+		
+		sc.close();
+
+	}
+
+}
